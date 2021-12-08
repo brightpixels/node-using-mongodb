d846427abbb99423a6d33276cb5ebaee9b422c96
Abdi <abdi.saeed@digital.homeoffice.gov.uk>
2021-12-08 19:57:50 +0000

    Completed module

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+mongoose.Promise = global.Promise;
+await mongoose.connect("mongodb://localhost/productsdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const app = express();
 const PORT = 4000;
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-app.get('/', (req, res) =>
-    res.send(`Store server running on port ${PORT}`)
-);
+app.get("/", (req, res) => res.send(`Store server running on port ${PORT}`));
 
-app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
